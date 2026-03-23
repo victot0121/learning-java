99a51f4018e37a0de77cc685d3ac133af52a53fc
victor oyiga <victoroyiga01@gmail.com>
2026-03-23 02:06:33 +0100

    working with variable on java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,9 @@
 class Hello{
    
     public static void main(String a[]){
-      System.out.print("hello world my first java code !");
+      int num1 = 2;
+      int num2 = 4;
+      System.out.println(num1 + num2);
     }
 
 }
