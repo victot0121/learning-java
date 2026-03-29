d7adfc3aea14d7e5104b847bc13b7d70721008a7
victor oyiga <victoroyiga01@gmail.com>
2026-03-29 14:30:04 +0100

    while loop

--- Hello.java
+++ Hello.java
@@ -1,15 +1,14 @@
-class Hello{
+class Hello {
+
+  public static void main(String a[]) {
+    // byte b = 125
+    int i = 1;
    
-    public static void main(String args[]){
-      //byte b = 125
-      int a = 257;
-      
-      if (a > 10 & a <= 20)
-
-      System.out.println("Hell0");
-
-      System.out.println("byb");
-      //System.out.print(t);
+    while (i <= 4) {
+      System.out.println(" Hello " + i);
+      i++;
     }
+
+  }
 
 }
